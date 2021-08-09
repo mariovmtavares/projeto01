008efb037233b8778482f8f0b6a25ef0857f5590
Mário Tavares <67693589+mariovmtavares@users.noreply.github.com>
2021-08-09 14:02:55 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,5 @@
+print("Bem-vindo à calculadora do Mário!")
+numero_1 = int(input("Por favor, insira o primeiro número: "))
+numero_2 = int(input("Agora o segundo número: "))
+
+print("Os números escolhisdos foram: ", numero_1, "e", numero_2)
